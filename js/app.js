6a54a8b60d322f3314830f65ede3d860538db2a4
KalsonSaint <saintkalson@gmail.com>
2020-01-30 16:55:48 +0100

    Updated

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const getMealBtn = document.getElementById('get_meal');
+const mealContainer = document.getElementById('meal');
+
+getMealBtn.addEventListener('click',()=>{
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        .then(res => res.json())
+        .then(res => {
+            createMeal(res.meals[0])
+        })
+});
+
+const createMeal = meal => {
+    mealContainer.innerHTML = `
+        <div class="row">
+            <div class="column five">
+                <img src="${meal.strMealThumb}" alt="Meal Image" />
+            </div>
+            <div class="column seven">
+                <h4>${meal.strMeal}</h4>
+                <p>${meal.strInstructions}</p>
+        </div>
+    `;
+}
