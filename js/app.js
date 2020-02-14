547a9fdcd0c729e0f68bde48182bb42f72a61555
KalsonSaint <saintkalson@gmail.com>
2020-02-14 13:57:06 +0100

    update

--- js/app.js
+++ js/app.js
@@ -13,25 +13,36 @@ const createMeal = meal => {
     const ingredients = [];
     for(i=1; i<=20; i++){
         if(meal[`strIngredient${i}`]){
-            ingredients.push(
-                `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
-            )
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+        } else {
+            break;
         }
     }
+
     mealContainer.innerHTML = `
         <div class="row">
-            <div class="column five">
+            <div class="columns five">
                 <img src="${meal.strMealThumb}" alt="Meal Image" />
                 <p><strong>Category:</strong> ${meal.strCategory}</p>
                 <p><strong>Area:</strong> ${meal.strArea}</p>
                 <p><strong>Tags:</strong> ${meal.strTags.split(',').join(', ')}</p>
                 
                 <h5>Ingredients</h5>
-                <ul></ul>
+                <ul>
+                    ${ingredients.map(ingredient => `
+                        <li>${ingredient}</li>
+                    `).join('')}
+                </ul>
             </div>
-            <div class="column seven">
+            <div class="columns seven">
                 <h4>${meal.strMeal}</h4>
                 <p>${meal.strInstructions}</p>
+            </div>
+        </div>
+        <div class="row>
+            <h5>Video Recipe</h5>
+            <div class="videoWrapper">
+                <iframe width = "500" height = "350" src = "https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}" / >
             </div>
         </div>
     `;
